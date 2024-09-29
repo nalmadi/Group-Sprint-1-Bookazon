0235e918d8140084ac93c70d8d54e696b95c6298
Syd4r <alipton909@gmail.com>
2024-09-29 19:41:00 -0400

    fixed item

--- src/Item.java
+++ src/Item.java
@@ -1,8 +1,6 @@
 public interface Item {
     public String getTitle();
-    public void setTitle(String title);
     public double getPrice();
-    public void setPrice(double price);
     public String toString();
     public boolean isPriceValid();
     public boolean isTitleValid();
