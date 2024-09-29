b458c2095b5e4535ae7546a771aea6cc9f782c0b
Delanyo Nutakor <dnutak26@colby.edu>
2024-09-28 22:28:32 -0400

    Refactor Order class: replace subscription string with User object, move discount logic to getDiscount method, and simplify address handling

--- src/Order.java
+++ src/Order.java
@@ -3,44 +3,18 @@ import java.util.ArrayList;
 public class Order {
     private String dateCreated;
     private String dateShipped;
-    private String userName;
+    private User user;
     private String orderStatus;
-    private String shippingAddressLine1;
-    private String shippingAddressLine2;
-    private String shippingAddressCity;
-    private String shippingAddressState;
-    private String shippingAddressZip;
-    private String shippingAddressCountry;
-    private String billingAddressLine1;
-    private String billingAddressLine2;
-    private String billingAddressCity;
-    private String billingAddressState;
-    private String billingAddressZip;
-    private String billingAddressCountry;
     private ArrayList<CartItem> items;
     private double orderPrice;
+    private int GOLD_DISCOUNT = 0.15;
+    private int PLATINUM_DISCOUNT = 0.10;
+    private int SILVER_DISCOUNT = 0.05; 
 
-    public Order(Cart cart, String subscription) {
+    public Order(Cart cart, User user) {
         this.items = cart.getItems();
-        this.orderPrice = calculatePrice(subscription);
-    }
-
-    public void setShippingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.shippingAddressLine1 = line1;
-        this.shippingAddressLine2 = line2;
-        this.shippingAddressCity = city;
-        this.shippingAddressState = state;
-        this.shippingAddressZip = zip;
-        this.shippingAddressCountry = country;
-    }
-
-    public void setBillingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.billingAddressLine1 = line1;
-        this.billingAddressLine2 = line2;
-        this.billingAddressCity = city;
-        this.billingAddressState = state;
-        this.billingAddressZip = zip;
-        this.billingAddressCountry = country;
+        this.user = user;
+        this.orderPrice = calculatePrice(user.subscription);
     }
 
     public void setOrderStatus(String status) {
@@ -55,34 +29,43 @@ public class Order {
         this.dateShipped = date;
     }
 
-    public void setUserName(String name) {
-        this.userName = name;
-    }
-
     public void printOrderDetails() {
         System.out.println("Order Details:");
         System.out.println("Date Created: " + dateCreated);
         System.out.println("Date Shipped: " + dateShipped);
-        System.out.println("User Name: " + userName);
+        System.out.println("User Name: " + user.name);
         System.out.println("Order Status: " + orderStatus);
-        System.out.println("Shipping Address: " + shippingAddressLine1 + ", " + shippingAddressLine2 + ", " + shippingAddressCity + ", " + shippingAddressState + ", " + shippingAddressZip + ", " + shippingAddressCountry);
-        System.out.println("Billing Address: " + billingAddressLine1 + ", " + billingAddressLine2 + ", " + billingAddressCity + ", " + billingAddressState + ", " + billingAddressZip + ", " + billingAddressCountry);
+        System.out.println("Shipping Address: " + user.shipping.line1 + ", " + user.shipping.line2 + ", " + user.shipping.city + ", " + user.shipping.state + ", " + user.shipping.zip + ", " + user.shipping.country);
+        System.out.println("Billing Address: " + user.billing.line1 + ", " + user.billing.line2 + ", " + user.billing.city + ", " + user.billing.state + ", " + user.billing.zip + ", " + user.billing.country);
+        System.out.println("Discount: " + getDiscount());
         System.out.println("Order Price: $" + orderPrice);
     }
 
-    public double calculatePrice(String subscription) {
+    public int getDiscount() {
+        if (user.subscription.equals("gold")) {
+            return GOLD_DISCOUNT;
+        } else if (user.subscription.equals("platinum")) {
+            return PLATINUM_DISCOUNT;
+        } else if (user.subscription.equals("silver")) {
+            return SILVER_DISCOUNT;
+        } else {
+            return 0;
+        }
+    }
+
+    public double calculatePrice() {
         double totalPrice = 0.0;
 
         for (CartItem item : items) {
             totalPrice += item.getTotalPrice();
         }
 
-        if (subscription == "gold") {
-            totalPrice *= 0.15; // 15% discount for prime members
-        } else if (subscription == "platinum") {
-            totalPrice *= 0.10; // 10% discount for platinum members
-        } else if (subscription == "silver") {
-            totalPrice *= 0.05; // 5% discount for silver members
+        if (user.subscription.equals("gold")) {
+            totalPrice *= (1 - GOLD_DISCOUNT); // 15% discount for prime members
+        } else if (user.subscription.equals("platinum")) {
+            totalPrice *= (1 - PLATINUM_DISCOUNT); // 10% discount for platinum members
+        } else if (user.subscription.equals("silver")) {
+            totalPrice *= (1 - SILVER_DISCOUNT); // 5% discount for silver members
         } 
 
         return totalPrice;
