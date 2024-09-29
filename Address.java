542ad4ab473f4898fa033aea1857d2d007c402eb
al_jisal <aljisal.frimpong@gmail.com>
2024-09-28 20:07:48 -0400

    created and added an Address class

---
+++ Address.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Address {
+
+    private String line1;
+    private String line2;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+
+    public Address(ArrayList<String> addressDetials){
+        try {
+            this.line1 = addressDetials.get(0);
+            this.line2 = addressDetials.get(1);
+            this.city = addressDetials.get(2);
+            this.state = addressDetials.get(3);
+            this.zip = addressDetials.get(4);
+            this.country = addressDetials.get(5);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("the address details list should have 6 elements");
+        }; 
+    }
+
+    public void setAddress(ArrayList<String> addressDetials) {
+        try {
+            this.line1 = addressDetials.get(0);
+            this.line2 = addressDetials.get(1);
+            this.city = addressDetials.get(2);
+            this.state = addressDetials.get(3);
+            this.zip = addressDetials.get(4);
+            this.country = addressDetials.get(5);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("the address details list should have 6 elements");
+        };
+    }
+}
