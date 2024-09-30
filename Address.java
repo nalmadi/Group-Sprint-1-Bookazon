03946eaf06a1a14c937ff4e040ab6b61d4d8333b
al_jisal <aljisal.frimpong@gmail.com>
2024-09-29 23:12:38 -0400

    Changed the constructor implementation to reuse setAddress method

--- Address.java
+++ Address.java
@@ -12,17 +12,7 @@ public class Address {
 
 
     public Address(ArrayList<String> addressDetials){
-        try {
-            this.line1 = addressDetials.get(0);
-            this.line2 = addressDetials.get(1);
-            this.city = addressDetials.get(2);
-            this.state = addressDetials.get(3);
-            this.zip = addressDetials.get(4);
-            this.country = addressDetials.get(5);
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("the address details list should have 6 elements");
-        }; 
+        setAddress(addressDetials); // Reuses the setAdddress method
     }
 
     public void setAddress(ArrayList<String> addressDetials) {
